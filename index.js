5970663f9b349b7847144d10524d225ecb2c1fba
fireonex <mari.krulikovskaya@mail.ru>
2024-05-09 16:11:47 +0300

    2nd commit (add info about Promise methods: all and allSettled)

--- index.js
+++ index.js
@@ -5,6 +5,18 @@
     //3) rejected - обещание не выполнено
 
 
+//Promise (с большой буквы):
+// Promise — это глобальный объект, который представляет
+// собой конструктор для создания новых промисов. Он предоставляет
+// несколько статических методов для работы с промисами в более обобщенном виде.
+
+//promise (с маленькой буквы):
+// Это экземпляр объекта Promise. Каждый такой объект представляет собой
+// асинхронную операцию, которая может завершиться успехом или ошибкой.
+// Экземпляры промисов имеют методы, которые позволяют обрабатывать завершение этих операций.
+
+
+
 //----------------------------------------------------------------------------------//
 //методы promise:
 
@@ -20,25 +32,65 @@ promise1.then((data) => {
 //pending
 const findUserInDataBase = (id) => {}
 
-const promice2 = findUserInDataBase(1)
+const promise2 = findUserInDataBase(1)
 //.........
 //.............
 //resolved or rejected
 //.........
-promice2.then(
+promise2.then(
     //если promise перейдёт в состояние resolved то эта функция выполнится
     (user) => {console.log(user)}
 )
 //......
 //.........
 //rejected
-promice2.catch(
+promise2.catch(
     //если promise перейдёт в состояние rejected то эта функция выполнится
     (error) => {console.log(error)}
 )
 
 //3) finally - метод, который выполняется когда promise или за-resolve-лся или за-reject-ился
-promice2.finally(
+promise2.finally(
 
     () => {console.log('finish')}
 )
+
+//методы Promise: -------------------------------------------------//
+
+//all и allSettled
+
+const otherPromise = Promise.all([promise1, promise2])
+//метод .all вернёт другой промис (otherPromise)
+//массив результатов которыми за-resolve-лись эти два промиса (results) приходит ↓↓↓
+
+otherPromise.then((results) => {
+    //когда otherPromise за-resolve-тся, он выполнит эту функцию,
+    //а за-resolve-тся он тогда, когда промисы переданные в статический
+    //метод .all за-resolve-ятся
+
+    let dataFromGoogle = results[0]
+    let userFromDataBase = results[1]
+    console.log('finish resolved promise' + dataFromGoogle + userFromDataBase)
+})
+
+otherPromise.catch(() => {
+    //если хоть один из промисов не resolve-тся, то не resolve-ятся все промисы
+    console.log('failed, try later')
+})
+
+
+//метод allSettled возвращает другой промис, который за-resolve-тся тогда, когда
+//promise1 и promise2 уйдут из состояние pending(ожидание), т.е. если один
+// из промисов не resolve-тся, то доступ к за-resolve-ленному можно будет получить
+const otherPromise2 = Promise.allSettled([promise1, promise2])
+
+otherPromise2.then((results) => {
+
+    let dataFromGoogle =
+        results[0].status === 'fulfilled' ? results[0] : {data: null}
+
+    let userFromDataBase =
+        results[1].status === 'fulfilled' ? results[1] : {data: results[1].reason}
+
+})
+
